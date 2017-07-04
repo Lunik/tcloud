a3578b5a6c638e807e05c03e28225fd8b8f79b56
lunik <guillaume.lunik@gmail.com>
2017-07-04 15:41:21 +0200

    add app base + service worker

---
+++ src/public/components/app.js
@@ -0,0 +1,11 @@
+/**
+ * Created by lunik on 04/07/2017.
+ */
+
+import React from 'react'
+
+export default class App extends React.Component {
+  render () {
+    return null
+  }
+}
--- src/public/index.js
+++ src/public/index.js
@@ -1,3 +1,11 @@
 /**
  * Created by lunik on 04/07/2017.
  */
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/app'
+import registerServiceWorker from './registerServiceWorker'
+
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
