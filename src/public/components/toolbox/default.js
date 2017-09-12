ed4e03d2b4a9aee92d4139ab51df066e353043cc
lunik <guillaume.lunik@gmail.com>
2017-09-12 18:52:37 +0200

    Toolbox are now well placed #33

--- src/public/components/toolbox/default.js
+++ src/public/components/toolbox/default.js
@@ -83,9 +83,10 @@ const style = {
     verticalAlign: 'bottom'
   },
   list: {
+    position: 'relative',
+    right: '115px',
+    width: '150px',
     backgroundColor: Color.darkGrey,
-    position: 'fixed',
-    right: '0px',
     padding: '0px',
     borderRadius: '2px',
     boxShadow: '0px 11px 15px -7px rgba(0, 0, 0, 0.2), 0px 24px 38px 3px rgba(0, 0, 0, 0.14), 0px 9px 46px 8px rgba(0, 0, 0, 0.12)'
