85f4e47c1e7460bff97b4ac32f5f9bfed1eee187
lunik <guillaume.lunik@gmail.com>
2017-09-17 19:03:18 +0200

    console log request

--- src/server/routes/module/enforceHttps.js
+++ src/server/routes/module/enforceHttps.js
@@ -48,6 +48,7 @@ export default function (options) {
           }
         } catch (err) {
           log.error(err)
+          log.error(JSON.stringify(req))
           next()
         }
       } else {
