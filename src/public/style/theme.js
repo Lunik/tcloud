e5bc68410e3ef80e7067eabf46766b266a3e7ab0
lunik <guillaume.lunik@gmail.com>
2017-11-28 21:40:08 +0100

    Include christmas theme

--- src/public/style/theme.js
+++ src/public/style/theme.js
@@ -1,12 +1,20 @@
 import DefaultColor from './themes/default/color'
+import ChristmasColor from './themes/christmas/color'
 
 const currentDate = new Date()
 const year = currentDate.getFullYear()
 
+const christmasDates = {
+  begin: new Date(`12/15/${year}`),
+  end: new Date(`01/15/${year + 1}`)
+}
+
 let Color
 
-if (false) {
-
+if (currentDate > christmasDates.begin && currentDate < christmasDates.end) {
+  require('./themes/christmas/color.css')
+  require('./themes/christmas/style.css')
+  Color = ChristmasColor
 } else {
   require('./themes/default/color.css')
   require('./themes/default/style.css')
---
+++ src/public/style/themes/christmas/color.js
@@ -0,0 +1,14 @@
+
+export default {
+  orange: '#203320',
+  red: '#f75353',
+  green: '#27AE60',
+  blue: '#5264AE',
+  lightBlue: '#7082C2',
+  grey: '#FFFFFF',
+  darkGrey: '#70AE6E',
+  black: '#1C1C1C',
+  white: '#FFFFFF',
+  transparentGrey: 'rgba(95, 95, 95, 0.5)',
+  lightGrey: '#BBBBBB'
+}
